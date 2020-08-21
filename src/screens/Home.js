d7a6772cd3aaa7026cca95f65c4a393cd9711ae9
adifdwimaulana <adifdwimaulana@gmail.com>
2020-08-21 18:37:44 +0700

    Fix Navigation on Home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,6 +4,8 @@ import MapView, { Marker } from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
 import MapViewDirections from 'react-native-maps-directions';
 import SearchableDropdown from 'react-native-searchable-dropdown';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 
 import { auth, db } from '../config';
 
@@ -24,7 +26,14 @@ const locations = [
     { id: 6, name: 'Mulyosari' },
 ]
 
-const API_KEY = 'AIzaSyDf8UOiJrm6eJVdZ3ZMJdKqczzrcC9jqms'
+const API_KEY = 'AIzaSyDf8UOiJrm6eJVdZ3ZMJdKqczzrcC9jqms';
+
+const parkiranInfo = [
+    {
+        id: 1,
+        imageUrl: '../assets'
+    }
+]
 
 export default class Home extends React.Component {
     constructor(props) {
@@ -35,8 +44,8 @@ export default class Home extends React.Component {
                 const myLocation = {
                     latitude: position.coords.latitude,
                     longitude: position.coords.longitude,
-                    latitudeDelta: 0.008,
-                    longitudeDelta: 0.008
+                    latitudeDelta: 0.045,
+                    longitudeDelta: 0.045
                 }
                 this.setState({ origin: myLocation })
             },
@@ -153,31 +162,81 @@ export default class Home extends React.Component {
                             ) : null
                     }
                 </MapView>
-                <SearchableDropdown
-                    onTextChange={text => console.log(text)}
-                    onItemSelect={item => this.handleLokasi(item)}
-                    textInputStyle={{
-                        borderBottomColor: "#000",
-                        borderBottomWidth: StyleSheet.hairlineWidth,
-                        fontSize: 16,
-                        paddingLeft: 16,
-                        backgroundColor: '#FAF9F8',
-                        marginHorizontal: 24,
-                        marginTop: 12,
-                        marginBottom: 4,
-                        borderRadius: 4,
-                        elevation: 8
-                    }}
-                    itemStyle={styles.itemStyle}
-                    itemTextStyle={{
-                        color: '#222',
-                    }}
-                    items={locations}
-                    defaultIndex={0}
-                    placeholder="Masukkan Tujuan"
-                    resetValue={false}
-                    underlineColorAndroid="transparent"
-                />
+                <View style={{ flexDirection: 'column', justifyContent: 'space-between' }}>
+                    <SearchableDropdown
+                        onTextChange={text => console.log(text)}
+                        onItemSelect={item => this.handleLokasi(item)}
+                        textInputStyle={{
+                            borderBottomColor: "#000",
+                            borderBottomWidth: StyleSheet.hairlineWidth,
+                            fontSize: 16,
+                            paddingLeft: 16,
+                            backgroundColor: '#FAF9F8',
+                            marginHorizontal: 24,
+                            marginTop: 12,
+                            marginBottom: 4,
+                            borderRadius: 4,
+                            elevation: 8
+                        }}
+                        itemStyle={styles.itemStyle}
+                        itemTextStyle={{
+                            color: '#222',
+                        }}
+                        items={locations}
+                        defaultIndex={0}
+                        placeholder="Masukkan Tujuan"
+                        resetValue={false}
+                        underlineColorAndroid="transparent"
+                    />
+
+                    <ScrollView
+                        style={{ height: height * 0.2, position: 'absolute', marginTop: height * 0.68, marginHorizontal: 20 }}
+                        horizontal={true}
+                        showsHorizontalScrollIndicator={false}
+                    >
+                        {
+                            parks ?
+                                parks.map((item) =>
+                                    <View style={styles.card}>
+                                        <Image
+                                            source={{
+                                                uri: `${item.imageUrl}`
+                                            }}
+                                            style={{ width: '100%', height: 70, borderTopLeftRadius: 4, borderTopRightRadius: 4 }}
+                                        />
+                                        <View style={styles.titleContainer}>
+                                            <Text style={styles.itemTitle}>{item.name}</Text>
+                                            {
+                                                item.type == 'motor' ?
+                                                    <Icon style={[{ color: '#725E5E' }]} size={18} name={'motorcycle'} />
+                                                    : <Icon style={[{ color: '#725E5E' }]} size={18} name={'car'} />
+                                            }
+                                        </View>
+                                        <View style={styles.itemContainer}>
+                                            <Text style={styles.empty}>Tersedia : {item.empty}</Text>
+                                            <Text style={styles.cost}>Tarif : {item.cost}</Text>
+                                        </View>
+                                        <TouchableOpacity
+                                            style={styles.btnDestination}
+                                            onPress={() => {
+                                                const lat = item.latitude;
+                                                const long = item.longitude;
+                                                const coordinate = {
+                                                    latitude: lat,
+                                                    longitude: long
+                                                }
+                                                this.setState({ destination: coordinate })
+                                                console.log(coordinate)
+                                            }}
+                                        >
+                                            <Icon style={[{ color: '#725E5E' }]} size={18} name={'arrow-right'} />
+                                        </TouchableOpacity>
+                                    </View>
+                                ) : null
+                        }
+
+                    </ScrollView>
+                </View>
             </View>
         )
     }
@@ -186,9 +245,9 @@ export default class Home extends React.Component {
 const styles = StyleSheet.create({
     container: {
         height,
+        width,
         backgroundColor: '#fff',
     },
-
     itemStyle: {
         marginHorizontal: 24,
         paddingVertical: 10,
@@ -198,5 +257,52 @@ const styles = StyleSheet.create({
         borderColor: '#bbb',
         borderBottomColor: "#000",
         borderBottomWidth: StyleSheet.hairlineWidth,
-    }
+    },
+    card: {
+        height: '100%',
+        width: 200,
+        backgroundColor: '#FAF9F8',
+        borderRadius: 4,
+        elevation: 8,
+        marginHorizontal: 4,
+    },
+    titleContainer: {
+        marginHorizontal: 8,
+        marginTop: 4,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    itemTitle: {
+        fontSize: 16,
+        fontWeight: '600',
+        color: '#725E5E'
+    },
+    itemContainer: {
+        marginHorizontal: 8,
+        marginTop: 2,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    empty: {
+        fontSize: 12,
+        fontWeight: '600',
+        color: '#725E5E'
+    },
+    cost: {
+        fontSize: 12,
+        fontWeight: '400',
+        color: '#725E5E'
+    },
+    btnDestination: {
+        marginHorizontal: 8,
+        marginTop: 2,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-end'
+    },
 })
